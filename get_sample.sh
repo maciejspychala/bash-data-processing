842c860517016db8d0afbe3527039be58226e3df
maciejspychala <mac.spychala@gmail.com>
2018-04-03 19:11:47 +0200

    get related data

--- get_sample.sh
+++ get_sample.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-head -n 2000 ../unique_tracks.txt > tracks.txt
-head -n 20000 ../triplets_sample_20p.txt > play.txt
+sep_to_space() { sed 's/<SEP>/ /g'; };
+
+head -n 20000 ../triplets_sample_20p.txt | sep_to_space > play.txt
+tracks_id=$(cut -d' ' -f2 play.txt)
+grep -aFf <(printf "%s\n" "${tracks_id[@]}") ../unique_tracks.txt | sed 's/ /_/g' | sep_to_space > tracks.txt
