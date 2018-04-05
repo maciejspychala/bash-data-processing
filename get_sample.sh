b8b1ff0896c7710faf7c7c903fd216293d77c5dc
maciejspychala <mac.spychala@gmail.com>
2018-04-05 19:05:54 +0200

    change epoch time to date

--- get_sample.sh
+++ get_sample.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sep_to_space() { sed 's/<SEP>/ /g'; };
+sep_to_space() {
+    sed 's/<SEP>/ /g'
+}
 
-head -n 20000 ../triplets_sample_20p.txt | sep_to_space > play.txt
+epoch_to_date() {
+    awk '{ $3 = strftime("%Y/%m", $3) } { print }'
+}
+
+head -n 20000 ../triplets_sample_20p.txt | sep_to_space | epoch_to_date > play.txt
 tracks_id=$(cut -d' ' -f2 play.txt)
 grep -aFf <(printf "%s\n" "${tracks_id[@]}") ../unique_tracks.txt | sed 's/ /_/g' | sep_to_space > tracks.txt
