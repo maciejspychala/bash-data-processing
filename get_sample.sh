4c7b12879ba9c24c266f4933690a6ce631c788d5
maciejspychala <mac.spychala@gmail.com>
2018-04-05 20:48:49 +0200

    sort play by song_id

--- get_sample.sh
+++ get_sample.sh
@@ -8,6 +8,6 @@ epoch_to_date() {
     awk '{ $3 = strftime("%Y/%m", $3) } { print }'
 }
 
-head -n 20000 ../triplets_sample_20p.txt | sep_to_space | epoch_to_date > play.txt
+head -n 20000 ../triplets_sample_20p.txt | sep_to_space | epoch_to_date | sort -k2 > play.txt
 tracks_id=$(cut -d' ' -f2 play.txt)
 grep -aFf <(printf "%s\n" "${tracks_id[@]}") ../unique_tracks.txt | sed 's/ /_/g' | sep_to_space | sort -k2 > tracks.txt
--- users_unique_tracks.sh
+++ users_unique_tracks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cut -d' ' -f -2 play.txt | sort -k2 | join -j2 - tracks.txt -o 1.1,2.1 | sort | uniq | cut -d' ' -f1 | uniq -c | sort -nr
+cut -d' ' -f -2 play.txt | join -j2 - tracks.txt -o 1.1,2.1 | sort | uniq | cut -d' ' -f1 | uniq -c | sort -nr
