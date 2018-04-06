87cc2ec69f40fd8363f60564c86a2c894720bda9
maciejspychala <mac.spychala@gmail.com>
2018-04-06 11:12:13 +0200

    multiline

--- best_artist.sh
+++ best_artist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export LC_ALL=C
-cut -d' ' -f 2 play.txt | uniq -c | join -j2 -o 1.1,2.3 - tracks.txt | awk '{a[$2] += $1}END{for(i in a) print a[i],i}' | sort -nr
+cut -d' ' -f 2 play.txt | uniq -c | join -j2 -o 1.1,2.3 - tracks.txt |\
+    awk '{a[$2] += $1}END{for(i in a) print a[i],i}' | sort -nr
