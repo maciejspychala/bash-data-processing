f8cfb2d00fce37b47e78bdca56ff469797c56a05
maciejspychala <mac.spychala@gmail.com>
2018-04-06 10:24:48 +0200

    top_song_by_artist.sh usage

--- top_song_by_artist.sh
+++ top_song_by_artist.sh
@@ -2,6 +2,10 @@
 
 export LC_ALL=C
 
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 ARTIST_NAME"
+fi
+
 grep -Ff <(grep " $1 " tracks.txt | cut -d' ' -f2 | sort -u) play.txt > /tmp/artist_plays
 tracks_id=$(cut -d' ' -f2 /tmp/artist_plays | uniq -c | sort -nr | head -n 3 | awk '{print $2}')
 grep -Ff <(printf "%s\n" "${tracks_id[@]}") /tmp/artist_plays | cut -d' ' -f -2 | sort -u | cut -d' ' -f1 | uniq -c| grep ' 3 ' | awk '{print $2}'
