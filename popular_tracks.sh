f0414813b819ee2f0bf5ee68891e97ec83a517bb
maciejspychala <mac.spychala@gmail.com>
2018-04-06 17:00:13 +0200

    improve popular_tracks

--- popular_tracks.sh
+++ popular_tracks.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cut -d' ' -f2 play.txt | sort | uniq -c | join -j2 - tracks.txt -o 1.1,1.2,2.3,2.4 | sort -nrk1
+export LC_ALL=C
+
+cut -d' ' -f2 play.txt | uniq -c | join -j2 - tracks.txt -o 1.1,1.2,2.3,2.4 | sort -nrk1
