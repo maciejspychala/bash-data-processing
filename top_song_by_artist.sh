87ebb767f77e87c6632f6dd7e87f476c36e9c586
maciejspychala <mac.spychala@gmail.com>
2018-04-06 10:20:52 +0200

    top song by artist

---
+++ top_song_by_artist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+grep -Ff <(grep " $1 " tracks.txt | cut -d' ' -f2 | sort -u) play.txt > /tmp/artist_plays
+tracks_id=$(cut -d' ' -f2 /tmp/artist_plays | uniq -c | sort -nr | head -n 3 | awk '{print $2}')
+grep -Ff <(printf "%s\n" "${tracks_id[@]}") /tmp/artist_plays | cut -d' ' -f -2 | sort -u | cut -d' ' -f1 | uniq -c| grep ' 3 ' | awk '{print $2}'
