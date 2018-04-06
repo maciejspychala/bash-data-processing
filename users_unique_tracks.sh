e7aff4e174890ea084a8b9612d9d498cfa3cf10a
maciejspychala <mac.spychala@gmail.com>
2018-04-06 17:13:15 +0200

    improve users_unique_tracks

--- users_unique_tracks.sh
+++ users_unique_tracks.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cut -d' ' -f -2 play.txt | join -j2 - tracks.txt -o 1.1,2.1 | sort | uniq | cut -d' ' -f1 | uniq -c | sort -nr
+export LC_ALL=C
+
+cut -d' ' -f -2 play.txt | sort -u | cut -d' ' -f1 | uniq -c | sort -nr
